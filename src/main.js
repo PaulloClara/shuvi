2b0a03f2e4469e4cd33268fed7df26be0fefe3f1
paulloclara <paulloclara@gmail.com>
2020-06-14 22:02:18 -0300

    config electron

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log("HI <3");
+const { join: resolvePath } = require("path");
+const { app, BrowserWindow } = require("electron");
+
+const windows = {};
+
+class AppWindow extends BrowserWindow {
+  constructor() {
+    super({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    this.loadFile(resolvePath(__dirname, "views", "index.html"));
+  }
+}
+
+app.on("ready", () => {
+  windows.main = new AppWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
